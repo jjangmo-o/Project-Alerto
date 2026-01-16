2bd77d90327da7f8522f7b19992ae03507b15663
jjangmyeonn <147785671+jjangmyeonn@users.noreply.github.com>
2026-01-17 07:50:25 +0800

    Fix: Restructure for Vercel serverless API functions

---
+++ api/index.js
@@ -0,0 +1,35 @@
+const { NestFactory } = require('@nestjs/core');
+const { ExpressAdapter } = require('@nestjs/platform-express');
+const express = require('express');
+
+// Import the compiled AppModule
+const { AppModule } = require('../backend/dist/app.module');
+const { HttpExceptionFilter } = require('../backend/dist/common/filters/http-exception.filter');
+
+const server = express();
+let app;
+
+async function bootstrap() {
+  if (!app) {
+    const nestApp = await NestFactory.create(
+      AppModule,
+      new ExpressAdapter(server),
+    );
+
+    nestApp.enableCors({
+      origin: '*',
+      methods: 'GET,POST,PUT,DELETE',
+    });
+
+    nestApp.useGlobalFilters(new HttpExceptionFilter());
+
+    await nestApp.init();
+    app = nestApp;
+  }
+  return server;
+}
+
+module.exports = async (req, res) => {
+  await bootstrap();
+  server(req, res);
+};
